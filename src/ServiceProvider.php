c7d34a21783fca07422078d8e9cc154cf7b41f14
Edward <>
2019-05-16 21:48:03 +0200

    initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Linguini;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/linguini.php', 'linguini'
+        );
+    }
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/linguini.php' => config_path('linguini.php'),
+        ]);
+    }
+}
